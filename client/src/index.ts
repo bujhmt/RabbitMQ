1dab34a6f462ac21fba6ac8121476aacc8fca8fb
Igor Fenchenko <igor.fenchenko@traffickitchen.io>
2022-01-12 12:54:46 +0200

    Tutorial 6 [https://www.rabbitmq.com/tutorials]

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,7 +2,7 @@ import {connect} from 'amqplib';
 import {Queue} from './enums/queue.enum';
 import {Environment} from './interfaces/environment.interface';
 import {getEnv} from './helpers/get-env.helper';
-import {parseArgument} from "./helpers/parse-argument.helper";
+import {parseArgument} from './helpers/parse-argument.helper';
 
 const RETRY_TIME = 1000;
 
@@ -16,24 +16,38 @@ async function main() {
 
     const channel = await connection.createChannel();
 
-    await channel.assertQueue(Queue.TASK_QUEUES, {
-        durable: true,
+    const callbackQueue = await channel.assertQueue('', {
+        exclusive: true,
     });
+
+    const correlationId = new Date().toISOString();
+
+    await channel.consume(callbackQueue.queue, (message) => {
+        if (message && message.properties.correlationId === correlationId) {
+            console.log(`Response received: "${message.content.toString()}"`)
+            setTimeout(() => {
+                channel.close();
+                process.exit(0);
+            });
+        }
+    })
 
     const interval = setInterval(() => {
         if (
             channel.sendToQueue(
-                Queue.TASK_QUEUES,
+                Queue.RPC,
                 Buffer.from(JSON.stringify({
                     time: parseInt(parseArgument('-t'), 10) || 0,
                     message: parseArgument('-m') || 'Ping!',
                 })),
                 {
+                    correlationId,
+                    replyTo: callbackQueue.queue,
                     persistent: true,
                 }
             )
         ) {
-            console.log('Message successfully sent!');
+            console.log('Message successfully sent!', '\nWaiting for response...');
             clearInterval(interval);
         } else {
             console.log('Failed to send a message! Retrying...')
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,8 @@ import {connect} from 'amqplib';
 import {Queue} from './enums/queue.enum';
 import {Environment} from './interfaces/environment.interface';
 import {parseMessage} from './helpers/parse-message.helper';
-import {Task} from "./interfaces/task.interface";
-import {getEnv} from "./helpers/get-env.helper";
+import {Task} from './interfaces/task.interface';
+import {getEnv} from './helpers/get-env.helper';
 
 const {RABBIT_PORT, RABBIT_USER, RABBIT_PASSWORD, RABBIT_HOST, PREFETCH_COUNT} = getEnv<Environment>();
 
@@ -11,30 +11,42 @@ async function main() {
     const url = `amqp://${RABBIT_USER}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_PORT}`;
 
     console.log(`Connecting to ${url}...`);
+
     const connection = await connect(url);
 
     const channel = await connection.createChannel();
 
-    const {queue, messageCount} = await channel.assertQueue(Queue.TASK_QUEUES, {
-        durable: true,
+    const {queue, messageCount} = await channel.assertQueue(Queue.RPC, {
+        durable: false,
     });
 
     console.log(`${queue} queue asserted! Total messages: ${messageCount}`);
+
     await channel.prefetch(parseInt(PREFETCH_COUNT, 10));
 
-    await channel.consume(Queue.TASK_QUEUES, (message) => {
+    await channel.consume(Queue.RPC, (message) => {
         if (message) {
             const task = parseMessage<Task>(message);
-            console.log(`[${Queue.TASK_QUEUES}] New message! Processing time: ${task.time}`);
+            console.log(`[${Queue.RPC}] New message! Processing time: ${task.time}`);
 
             new Promise<string>((resolve) => {
                 setTimeout(() => {
-                    channel.ack(message);
                     resolve(task.message);
                 }, task.time * 1000);
             })
-                .then(
-                    (result) => console.log(`[${Queue.TASK_QUEUES}] Message processed: ${result}`)
+                .then((result) => {
+                        if (message.properties.replyTo) {
+                            channel.sendToQueue(
+                                message.properties.replyTo,
+                                Buffer.from(`Echo: ${result}`),
+                                {
+                                    correlationId: message.properties.correlationId
+                                }
+                            );
+                        }
+
+                        channel.ack(message);
+                    }
                 )
                 .catch(() => {
                     channel.nack(message);
